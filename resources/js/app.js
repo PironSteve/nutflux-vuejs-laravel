7e525fae87beb94c941581be051da9311aa2b942
lau-bel <l.belferroum@gmail.com>
2019-01-07 14:13:04 +0100

    laura

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,9 +20,11 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('navbar', require('./components/Navbar.vue'));
-Vue.component('general-movies', require('./components/GeneralMovies.vue'));
+Vue.component('navbar', require('./components/Navbar.vue').default);
+Vue.component('general-movies', require('./components/GeneralMovies.vue').default);
+Vue.component('edit-profile', require('./components/EditProfile.vue').default);
+Vue.component('pop-up-vid', require('./components/PopUpVid.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
